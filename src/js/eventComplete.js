3a693d109c3b9cd486743dc85d55480b4c7827c1
Wael Elsafty <waelelsafty07@gmail.com>
2023-05-11 17:38:19 +0300

    Fix function eventComplete & removeTask

--- src/js/eventComplete.js
+++ src/js/eventComplete.js
@@ -1,24 +1,5 @@
 import List from './list.js';
 
-const eventComplete = () => {
-  const listItems = document.querySelectorAll('.to-do-list-item');
-  listItems.forEach((item) => {
-    item.addEventListener('change', () => {
-      const list = new List();
-      const index = item.querySelector('a').getAttribute('index');
-      const span = item.querySelector('span');
-      if (span) {
-        span.classList.toggle('completed');
-        if (span.classList.contains('completed')) {
-          list.UpdateCompleteTask(Number(index));
-        } else {
-          list.UpdateUnCompleteTask(Number(index));
-        }
-      }
-    });
-  });
-};
-
 const addEventComplete = (item) => {
   item.addEventListener('change', () => {
     const list = new List();
@@ -34,4 +15,12 @@ const addEventComplete = (item) => {
     }
   });
 };
+
+const eventComplete = () => {
+  const listItems = document.querySelectorAll('.to-do-list-item');
+  listItems.forEach((item) => {
+    addEventComplete(item);
+  });
+};
+
 export { eventComplete, addEventComplete };
--- src/js/removeTask.js
+++ src/js/removeTask.js
@@ -1,21 +1,5 @@
 import { edit, deleteItem } from './clickCount.js';
 
-const buttonAction = () => {
-  const buttonTasks = document.querySelectorAll('.btn-task');
-
-  buttonTasks.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      e.preventDefault();
-      const icon = btn.querySelector('i');
-      if (icon.classList.contains('fa-trash-can')) {
-        deleteItem(icon, btn);
-      }
-      if (icon.classList.contains('fa-ellipsis-vertical')) {
-        edit(icon, btn);
-      }
-    });
-  });
-};
 const addNewActionWhenTaskAdded = (btn) => {
   btn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -26,6 +10,14 @@ const addNewActionWhenTaskAdded = (btn) => {
     if (icon.classList.contains('fa-ellipsis-vertical')) {
       edit(icon, btn);
     }
+  });
+};
+
+const buttonAction = () => {
+  const buttonTasks = document.querySelectorAll('.btn-task');
+
+  buttonTasks.forEach((btn) => {
+    addNewActionWhenTaskAdded(btn);
   });
 };
 
